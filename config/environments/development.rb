6ebdef2aaab909b85767f2a659fc31d5c4425a1e
You Matsumoto <anata0814@gmail.com>
2017-12-17 11:22:59 +0900

    email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,16 @@ Rails.application.configure do
 
   # defailt url option for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: Rails.application.secrets.smtp_username,
+    password: Rails.application.secrets.smtp_password,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
